012d0d08392b4b8e0d9ff82f3ec924775e0ea439
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-17 19:50:00 +0000

    main JS

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -3,7 +3,7 @@ let music = new Audio("music.mp3")
 let audioTurn = new Audio("ring.mp3")
 let gameover = new Audio("Gameover.mp3")
 let turn = "X"
-let isgameover = false;
+// let isgameover = false;
 
 // Function to change the turn
 const changeTurn = ()=>{
@@ -24,28 +24,19 @@ const changeTurn = ()=>{
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            audioTurn.play();
-            checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
-        }
-    })
+element.addEventListener('click', () => {
+    if (boxtext.innerText === '') {
+        boxtext.innerText = turn;
+
+        audioTurn.currentTime = 0; // reset sound
+        audioTurn.play();          // play sound immediately
+
+        turn = changeTurn();       // THEN change turn
+
+        document.getElementsByClassName("info")[0].innerText =
+            "Turn for " + turn;
+    }
+});
+
 })
 
-// Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
-    });
-    turn = "X"; 
-    isgameover = false
-    document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
