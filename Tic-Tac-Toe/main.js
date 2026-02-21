1b9253add3eff399d30c62885ab4bc5abf3e33b7
Tarang Tank <167697814+Tarang182045@users.noreply.github.com>
2026-02-21 15:12:34 +0000

    main JS

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -16,14 +16,14 @@ const changeTurn = () => {
 const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
     ];
 
     wins.forEach(e => {
@@ -39,6 +39,9 @@ const checkWin = () => {
             gameover.play();
 
             document.querySelector('.imgbox video').style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
+
 
         }
     });
@@ -79,4 +82,9 @@ reset.addEventListener('click', () => {
     isgameover = false;
     document.querySelector(".info").innerText = "Turn for X";
     document.querySelector('.imgbox video').style.width = "0px";
+    document.querySelector(".line").style.width = "0vw";
 });
+
+
+
+
